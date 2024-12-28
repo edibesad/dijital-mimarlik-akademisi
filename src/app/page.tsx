9f00b4d05a08cfbb5017b96e80feefbc3e98bfcd
edibesad <edibesad@gmail.com>
2024-12-28 10:20:30 +0000

    using products started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import InformationTexts from "./components/information_texts/InformationTexts";
 import ScrollSlider from "./components/scroll_slider/ScrollSlider";
 import SmoothScrolling from "./components/SmoothScrolling";
+import UsingProducts from "./components/using_products/UsingProducts";
 import WelcomeVideo from "./components/welcome_video/WelcomeVideo";
 
 export default function Home() {
@@ -15,7 +16,8 @@ export default function Home() {
         <ScrollSlider />
         <div className="bg-pink-700 h-screen">
           <div className="container"></div>
-      </div>
+        </div>
+        <UsingProducts />
       </SmoothScrolling>
     </>
   );
