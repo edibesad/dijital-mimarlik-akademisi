7d047c06248a2fbaf5cb6ca99c573a024b327404
Edib Esad Galip <edibesad@gmail.com>
2025-01-21 20:06:10 +0300

    fix: update social media button links

--- src/app/components/buttons/social_media_buttons/social_media_buttons.tsx
+++ src/app/components/buttons/social_media_buttons/social_media_buttons.tsx
@@ -11,11 +11,23 @@ import SocailMediaButtonElement from "./social_media_button_element";
 import SocialMediaButtonElementModel from "./social_media_button_element_model";
 
 const elements = [
-  new SocialMediaButtonElementModel(faTelegram, "https://t.me/serif"),
-  new SocialMediaButtonElementModel(faWhatsapp, "https://wa.me/+90"),
-  new SocialMediaButtonElementModel(faInstagram, "https://instagram.com/serif"),
-  new SocialMediaButtonElementModel(faYoutube, "https://youtube.com/serif"),
-  new SocialMediaButtonElementModel(faEnvelope, "mailto:"),
+  new SocialMediaButtonElementModel(
+    faTelegram,
+    "https://t.me/+AzEU7wFf9chkYjg0"
+  ),
+  new SocialMediaButtonElementModel(faWhatsapp, "https://wa.me/+905444942916"),
+  new SocialMediaButtonElementModel(
+    faInstagram,
+    "https://www.instagram.com/dijitalmimarlikakademisi"
+  ),
+  new SocialMediaButtonElementModel(
+    faYoutube,
+    "https://www.youtube.com/@muhammedmusabersan"
+  ),
+  new SocialMediaButtonElementModel(
+    faEnvelope,
+    "mailto:dijitalmimarlikakademisi@gmail.com"
+  ),
 ];
 
 export default function SocialMediaButtons() {
